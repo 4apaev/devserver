7adcbef9c26aec05e283f2206eb9faf94b4fb22f
michael <4apaev@gmail.com>
2016-01-15 20:17:20 +0200

    add body parser

---
+++ src/body.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+module.exports = parseBody
+function parseBody(req, res, next) {
+  let body = ''
+  req.setEncoding('utf8')
+  req.on('data', chunk => body += chunk)
+  req.on('end', () => {
+    try {
+      req.body = JSON.parse(body);
+    } catch(e) {
+      res.statusCode = 400;
+      return res.end(e.message);
+    }
+    next()
+  })
+}
