6a845d968cd801b069cf2dc1fe26d2f0956ecdd0
michael <4apaev@gmail.com>
2016-01-15 07:06:34 +0200

    add logger

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.server = require('./src/server');
-module.exports.statiq = require('./src/static')(__dirname);
-module.exports.brws = require('./src/brws')('./pub/main.js');
+module.exports.statiq = require('./src/static');
+module.exports.brws = require('./src/brws');
+module.exports.logger = require('./src/logger');
