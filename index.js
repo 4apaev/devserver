b434dd0d283944a873187d6c293f67a3c42e8c18
michael <4apaev@gmail.com>
2016-03-16 15:48:10 +0200

    add favicon middleware

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.server = require('./src/server');
 exports.statiq = require('./src/static');
 exports.logger = require('./src/logger');
 exports.body = require('./src/body');
+exports.favicon = require('./src/favicon');
