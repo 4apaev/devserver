cea7b03053d2ab40f2f4c3d11fe718508b13b7e1
michael <4apaev@gmail.com>
2016-03-19 05:45:49 +0200

    es6 format

--- src/body.js
+++ src/body.js
@@ -1,11 +1,9 @@
-
 'use strict';
 
-module.exports = parseBody
-function parseBody(req, res, next) {
-  let body = ''
-  req.setEncoding('utf8')
-  req.on('data', chunk => body += chunk)
+module.exports = (req, res, next) => {
+  let body = '';
+  req.setEncoding('utf8');
+  req.on('data', chunk => body += chunk);
   req.on('end', () => {
     try {
       req.body = JSON.parse(body);
