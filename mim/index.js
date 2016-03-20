74e4b1cff52b51519e2155fb1f694e034a6f32f5
michael <4apaev@gmail.com>
2016-03-20 04:54:18 +0200

    remove define and simplify set methods

--- mim/index.js
+++ mim/index.js
@@ -7,36 +7,21 @@ class Mim {
   }
   alias(a,b) {
     for(let i=0; i < b.length; i++)
-      this.set(b[i], a)
-    return this;
-  }
-  define(map) {
-    for(let a in map)
-      this.alias(a, map[a])
+      this.set(b[i], a);
     return this;
   }
   get(x, def) {
     return this.attrs[x.split('.').pop()]||def;
   }
   set(a,b) {
-    if('object'===typeof a)
-      for(let k in a)
-        this.set(k, a[k]);
-    else
-      a && (this.attrs[a] = b);
+    Object.assign(this.attrs, 'object'===typeof a ? a : {[a]:b});
     return this;
   }
   is(x) {
     return this.get(x,'').split('/')[0]
   }
-  isApp(x)  { return 'application'==this.is(x) }
-  isText(x) { return 'text'  == this.is(x) }
-  isFont(x) { return 'font'  == this.is(x) }
-  isAudio(x){ return 'audio' == this.is(x) }
-  isVideo(x){ return 'video' == this.is(x) }
-  isImage(x){ return 'image' == this.is(x) }
 }
 
 let mim = new Mim;
-mim.define(types);
+Object.keys(types).forEach(t => mim.alias(t, types[t]));
 module.exports = mim;
