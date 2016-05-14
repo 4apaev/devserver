7010012810273ad7882eaf485dc5661ba2f6d3b4
michael <4apaev@gmail.com>
2016-05-15 01:06:07 +0300

    log uptime

--- src/logger.js
+++ src/logger.js
@@ -1,15 +1,16 @@
 'use strict';
+let convert = require('readdate')
+let up = new Date;
 module.exports = (req, res, next) => {
-
   let start = new Date;
   res.once('finish', () => {
-    // start.toLocaleTimeString(),
-    console.log(new Date - start,
-                ' | ',
-                start.toLocaleTimeString(),
+    let now = Date.now();
+    console.log(start.toLocaleTimeString('en-us', { hour12: false }),
                 res.statusCode,
                 req.method,
-                req.url)
+                req.url,
+                'took', now-start,
+                'up for', convert(now-up))
   });
   next();
 }
