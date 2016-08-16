02af9534c42d035b6735f18013854e0b50cf396a
michael <4apaev@gmail.com>
2016-08-16 03:33:53 +0300

    fix typo

--- middleware/static.js
+++ middleware/static.js
@@ -1,22 +1,18 @@
 'use strict';
-let Path = require('path');
 let fs = require('fs');
+let Path = require('path');
 let mim = require('./mim/index');
 
 module.exports = (base, maxage=86400000) => (req, res) => {
-  
   let file = Path.join(base, req.url);
   
   fs.stat(file, (err, stats) => {
     if (!err && stats.isFile()) {
-      
       res.statusCode = 200;
       res.setHeader('Content-Length', stats.size);
-
       res.setHeader('Cache-Control', `public, max-age=${maxage}`);
       res.setHeader('Expires', new Date(Date.now() + maxage).toUTCString());
-      
-      res.setHeader('Content-Type', mim.get(x,'plain/text'));
+      res.setHeader('Content-Type', mim.get(file,'plain/text'));
       fs.createReadStream(file).pipe(res);
     }
     else{
