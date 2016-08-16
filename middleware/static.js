e2ea4a399c2871bb09f86b3a53a04dfcd09901f7
michael <4apaev@gmail.com>
2016-08-16 03:23:31 +0300

    add cache control options

--- middleware/static.js
+++ middleware/static.js
@@ -3,13 +3,21 @@ let Path = require('path');
 let fs = require('fs');
 let mim = require('./mim/index');
 
-module.exports = base => (req, res) => {
-  let x = Path.join(base, req.url);
-  fs.stat(x, (err, s) => {
-    if(!err && s.isFile()){
+module.exports = (base, maxage=86400000) => (req, res) => {
+  
+  let file = Path.join(base, req.url);
+  
+  fs.stat(file, (err, stats) => {
+    if (!err && stats.isFile()) {
+      
       res.statusCode = 200;
+      res.setHeader('Content-Length', stats.size);
+
+      res.setHeader('Cache-Control', `public, max-age=${maxage}`);
+      res.setHeader('Expires', new Date(Date.now() + maxage).toUTCString());
+      
       res.setHeader('Content-Type', mim.get(x,'plain/text'));
-      fs.createReadStream(x).pipe(res);
+      fs.createReadStream(file).pipe(res);
     }
     else{
       res.statusCode = 404;
