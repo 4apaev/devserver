d8c9d5e44907ec1a0511c8cad3a9a15700c287d9
michael <4apaev@gmail.com>
2016-08-16 03:22:35 +0300

    check content type before parsing req body

--- middleware/body.js
+++ middleware/body.js
@@ -2,11 +2,12 @@
 
 module.exports = (req, res, next) => {
   let body = '';
+  let isJson = 'application/json' == req.headers['content-type']
   req.setEncoding('utf8');
   req.on('data', chunk => body += chunk);
   req.on('end', () => {
     try {
-      req.body = JSON.parse(body);
+      req.body = isJson ? JSON.parse(body) : body;
       next();
     } catch(e) {
       res.statusCode = 400;
