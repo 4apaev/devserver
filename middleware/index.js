799224719447473c68dc1e6501295e9fbebfa51c
michael <4apaev@gmail.com>
2016-08-16 15:40:57 +0300

    export all

---
+++ middleware/index.js
@@ -0,0 +1,6 @@
+exports.body     = require('./body')
+exports.favicon  = require('./favicon')
+exports.logger   = require('./logger')
+exports.mim      = require('./mim')
+exports.query    = require('./query')
+exports.statiq   = require('./static')
